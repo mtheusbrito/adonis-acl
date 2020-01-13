946797eb64f6e5621aaef28df58577b31c53ac1e
matheus <matheusbritodasilva@gmail.com>
2020-01-13 09:19:24 -0300

    Ajustando o UserController

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,6 +1,45 @@
-'use strict'
+"use strict";
 
 class UserController {
+  async store({ request }) {
+    const { permissions, roles, ...data } = request.only([
+      "username",
+      "email",
+      "password",
+      "permissions",
+      "roles"
+    ]);
+    const user = await User.create(data);
+    if (roles) {
+      await user.roles().attach(roles);
+
+    }
+    if(permissions){
+      await user.permissions().attach(permissions)
+
+    }
+
+
+    await user.loadMany(['roles', 'permissions'])
+    return user;
+  }
+  async update({ request, params }) {
+    const data = request.only(["username", "email", "password"]);
+    const user = await User.findOrFail(params.id);
+    user.merge(data);
+    await user.save();
+
+    if (roles) {
+      await user.roles().sync(roles);
+
+    }
+    if(permissions){
+      await user.permissions().sync(permissions)
+
+    }
+    await user.loadMany(['roles', 'permissions'])
+    return user;
+  }
 }
 
-module.exports = UserController
+module.exports = UserController;
